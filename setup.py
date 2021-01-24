bd20a377ae295cbf547272f60c8877cc7f09ec39
tatarize <tatarize@gmail.com>
2021-01-23 22:35:05 -0800

    Update Version, add extras.

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@ setup(
     install_requires=[
         "meerk40t>=0.7.0-post11",
     ],
+    extras_require={
+        'cv': ["opencv-python-headless>=3.4.0.0"],
+        'cvhead': ["opencv-python>=3.4.0.0"],
+    }
 )
